02c92c444733a20f5ec62800bdad32e19e1be1c6
Carlos Carvallo <carloscarvallo87@gmail.com>
2020-06-18 14:17:47 -0400

    chore: clean example

--- public/main.js
+++ public/main.js
@@ -226,7 +226,6 @@ async function main() {
 
   startCallBtn.addEventListener('click', () => {
     if (tokySession) {
-      debugger
       tokySession.acceptCall()
       setupSessionListeners(tokySession)
     }
