7b4c3d136dbd1f64d3ef981ebc3cb630b6753a54
Carlos Carvallo <carloscarvallo87@gmail.com>
2020-06-26 17:31:45 -0400

    feat(SIP.js): comment external conf for sip.js

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) => {
        * SIP.js would be treated as a peer dependency
        * to reduce bundle size
        */
-      externals: ['sip.js'],
+      // externals: ['sip.js'],
     },
     modeConfig(mode)
   )
