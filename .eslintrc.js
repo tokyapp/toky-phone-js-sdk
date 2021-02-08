dc3da966bda95484b7cb34345092e3e8c0b97e87
carloscarvallo <carloscarvallo87@gmail.com>
2021-02-08 16:32:43 -0300

    chore: some fixes for eslint conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,17 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es6: true,
+  },
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:jsdoc/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
@@ -12,5 +20,17 @@ module.exports = {
   rules: {
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
   },
+  // ref: https://github.com/typescript-eslint/typescript-eslint/issues/964#issuecomment-569481854
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['warn'],
+      },
+    },
+  ],
+  plugins: ['jsdoc', '@typescript-eslint', 'prettier'],
 }
